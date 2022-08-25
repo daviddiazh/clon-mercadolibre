76b51f41540b56d9a3db20277525075caf141d79
daviddiazh <daviddiazlds1221@gmail.com>
2022-08-25 12:25:47 -0500

    Finished first part of navbar

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,6 +1,7 @@
 import React, { FC } from 'react'
 import { Box } from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
+import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
 import meliLogo from '../../assets/meli-logo.png';
 import meliLogoLarge from '../../assets/meli-large-logo.png';
 import disneyMeli from '../../assets/disney-meli.webp';
@@ -9,19 +10,24 @@ import './header.css';
 export const Header: FC = () => {
     return (
         <Box className="main-header">
-            <Box className='header-1'>
-                <img src={meliLogoLarge} className='logo-desktop' />
-                <img src={meliLogo} className='logo-mobile' />
+            <nav className="header">
+                <Box className='header-1'>
+                    <img src={meliLogoLarge} className='logo-desktop' />
+                    <img src={meliLogo} className='logo-mobile' />
 
-                <input 
-                    type="text" 
-                    name="search"
-                    placeholder="Buscar productos, marcas..."
-                    className="input-header"
-                />
-            </Box>
-            <img src={disneyMeli} alt="Disney Meli" className="img-disney-header" />
-            <span className='menu-icon'><MenuIcon /></span>
+                    <input 
+                        type="text" 
+                        name="search"
+                        placeholder="Buscar productos, marcas y más..."
+                        className="input-header"
+                    />
+                </Box>
+                <img src={disneyMeli} alt="Disney Meli" className="img-disney-header" />
+                <Box display='flex'>
+                    <span className='menu-icon'><MenuIcon /></span>
+                    <span className='menu-icon'><ShoppingCartOutlinedIcon /></span>
+                </Box>
+            </nav>
         </Box>
     )
 }
