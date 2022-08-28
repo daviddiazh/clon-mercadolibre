f4b7e9fa4a01ebbf432e295be55695dc5880fb23
daviddiazh <daviddiazlds1221@gmail.com>
2022-08-28 18:17:50 -0500

    Finished slider - with setTimeout for deslice every images

---
+++ src/components/Slider/Slider.tsx
@@ -0,0 +1,53 @@
+import React, { useState, useEffect, useRef } from 'react';
+import { FcNext, FcPrevious } from 'react-icons/fc';
+import './slider.css';
+
+export const Slider = ({ sliderImages }: any) => {
+
+    const [ currentImage, setCurrentImage ] = useState(0);
+
+    if( !Array.isArray(sliderImages) || sliderImages.length === 0 ) return;
+
+    const onNextImage = () => {
+        setCurrentImage( currentImage === sliderImages.length - 1 ? 0 : currentImage + 1 );
+    }
+
+    const onPreviousImage = () => {
+        setCurrentImage( currentImage === 0 ? sliderImages.length - 1 : currentImage - 1 );
+    }
+
+    useEffect(() => {
+
+        const timer = setTimeout(() => {
+            console.log('CAMBIO DE IMAGE - SLIDER')
+            onNextImage();
+        }, 5000)
+        
+        return () => {
+            console.log('DESMONTAJE DE IMAGEN')
+            clearTimeout(timer);
+        }
+    }, [ currentImage ]);
+
+
+    return (
+        <>
+            <div className="container-slider">
+                <button onClick={ onPreviousImage } className="btn-slider"><FcPrevious /></button>
+                {
+                    sliderImages.map((image, index) => (
+                        <div>
+                            {
+                                currentImage === index && (
+                                    <img key={ index } src={ image } alt="Slider Image" className="slide" />
+                                )
+                            }
+                        </div>
+                    ))
+                }
+                <button onClick={ onNextImage } className="btn-slider"><FcNext /></button>
+            </div>
+            
+        </>
+    )
+}
---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import { Slider } from '../components/Slider/Slider';
+import '../components/pages/Home.css';
+
+export const Home = () => {
+
+    const sliderImages = ['https://drive.google.com/uc?export=view&id=1u6KWKFUGMzuwTKJCom2koa1dUqXHIEJN', 'https://drive.google.com/uc?export=view&id=1K_-ZQVZiY2EFGPr8JAefNl-98KG7qh0s', 'https://drive.google.com/uc?export=view&id=1N0BroYVUbp8HIm4f9NS-8Dl-IpmhsNfq', 'https://drive.google.com/uc?export=view&id=1Y7UX5YgnjrPAktu8kitU8aYDfiVxovtU', 'https://drive.google.com/uc?export=view&id=1JRFTT_wtiwHlIzkUonbCzwdK4-MFjMJN', 'https://drive.google.com/uc?export=view&id=1Ubw3YdJ1JJZ8kquW637Eu01jgKOL0qTq', 'https://drive.google.com/uc?export=view&id=1Q5OhLmcQkmtZOk7bmsbcfZapWEihXgE2'];
+
+    return (
+        <main>
+            <Slider sliderImages={ sliderImages } /> 
+        </main>
+    )
+}
