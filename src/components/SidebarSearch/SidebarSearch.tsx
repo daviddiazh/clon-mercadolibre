17a8bea037cf17cd29c02f5b97ba2b4d86318f6e
daviddiazh <daviddiazlds1221@gmail.com>
2022-09-02 20:13:13 -0500

    Styles to search page

--- src/components/SidebarSearch/SidebarSearch.tsx
+++ src/components/SidebarSearch/SidebarSearch.tsx
@@ -6,13 +6,11 @@ export const SidebarSearch = ({ locationSplit = '' }) => {
 
     const { data, isLoading, hasError } = useFetch(`https://api.mercadolibre.com/sites/MLC/search?q=${ locationSplit }&limit=25`);
 
-    // if( isLoading ) return (<div style={{ width: '30vw', height: '100vw' }}><Loading /></div>)
+    if( isLoading ) return <Loading />
 
     return (
         <div className="container-sidebar-search">
-            {/* <div style={{ position: 'absolute', top: '50%', left: '25%' }}><Loading /></div> */}
-
-            <Loading />
+            <h1>hola mundo</h1>
         </div>
     )
 }
--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -3,6 +3,7 @@ import { useLocation } from 'react-router-dom';
 import { useFetch } from '../hooks/useFetch';
 import { SidebarSearch } from '../components/SidebarSearch/SidebarSearch';
 import { Loading } from '../components/Loading/Loading';
+import '../components/pages/Search.css';
 
 
 export const Search = () => {
@@ -18,33 +19,47 @@ export const Search = () => {
     console.log('data: ', data)
 
 
-    if( isLoading ) return <h1>Loading...</h1>
+    if( isLoading ) return <Loading />
 
     return (
-        <div>
-            <div>
-                <SidebarSearch />
-            </div>
-            <div>
-                {
-                    data && data?.results?.map(product => (
-                        <div>
-                            <img src={ product?.thumbnail } alt="" />
-                            <p>{product?.title}</p>
-                            <p>${ product?.price }</p>
-                            <p>{ product?.shipping?.free_shipping ? 'Envío gratis' : null }</p>
-                            <div>
-                                {
-                                    rate.map(item => (
-                                        <span style={{ color: "#2667ca" }}>
-                                            <AiFillStar />
-                                        </span>
-                                    ))
-                                }
+        <div className="general-container-search-page">
+            <div className="container-sidebar-and-search">
+                <div className="container-sidebar">
+                    <SidebarSearch />
+                </div>
+                <div className="container-search-page">
+                    {
+                        data && data?.results?.map(product => (
+                            <div className="one-product-list">
+                                <div>
+                                    <img className="photo-product" src={ product?.thumbnail } alt={ product?.title } />
+                                </div>
+                                <div>
+                                    <p style={{ fontWeight: 300, fontSize: '1.3rem', marginTop: '3px' }}>{product?.title}</p>
+                                    <div style={{ display: 'flex', alignItems: 'center', lineHeight: '5px', justifyContent: 'space-between', width: '350px', marginTop: '-10px' }}>
+                                        <div>
+                                            <p style={{ fontWeight: 500, fontSize: '1.6rem', marginTop: '15px', color: '#000', }}>$ { product?.price }</p>
+                                            <p style={{ fontWeight: 400, fontSize: '0.9rem', marginTop: '-6px', color: '#000', }}>en 36x $ {`${
+                                                String(product?.price! / 36).split('.')[0]
+                                            }`}</p>
+                                        </div>
+                                        <div>
+                                            {
+                                                rate.map(item => (
+                                                    <span style={{ color: "#3483fa" }}>
+                                                        <AiFillStar />
+                                                    </span>
+                                                ))
+                                            }
+                                            <p style={{ fontWeight: 300, fontSize: '0.7rem', marginTop: '18px', color: '#000', }}>Disponible</p>
+                                        </div>
+                                    </div>
+                                    <p style={{ fontWeight: 600, fontSize: '0.9rem', marginTop: '0px', color: '#00a650', }}>{ product?.shipping?.free_shipping ? 'Envío gratis' : null }</p>
+                                </div>
                             </div>
-                        </div>
-                    ))
-                }
+                        ))
+                    }
+                </div>
             </div>
         </div>
     )
