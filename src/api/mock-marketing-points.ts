f5e5470724bf7587baee583d8e47aafad8f12cac
daviddiazh <daviddiazlds1221@gmail.com>
2022-09-04 17:05:02 -0500

    Change paths imgs

--- src/api/mock-marketing-points.ts
+++ src/api/mock-marketing-points.ts
@@ -1,24 +1,38 @@
+import profileBrandDisneyStar from '../assets/logos-brands/comboplus-square.jpg';
+import profileBrandParamount from '../assets/logos-brands/paramount-logo-vdp-56-x-56-filled@2x.png';
+import profileBrandHBO from '../assets/logos-brands/logoSquare@2x.png';
+
+import bgDisneyAndStar from '../assets/benefits/disney-nivel6-mla-mco-mlm@2x.png';
+import bgParamount from '../assets/benefits/paramount-mco-mlm@2x.jpg';
+import bgHBO from '../assets/benefits/HBOMax_widgetmulti_mobdsk_mco_@2x.webp';
+
 export const mockMarketingPoints = [
     {
         id: 1,
-        backgroundPhoto: 'https://drive.google.com/uc?export=view&id=1pm9A5PV-a-VKA1r5Qv1GWN4UzbLipIuq',
-        profilePhotoBrand: 'https://drive.google.com/uc?export=view&id=1M2jeNUiF4PlXHauwL1tveCW3u70kHRuW',
+        backgroundPhoto: bgDisneyAndStar,
+        // backgroundPhoto: 'https://drive.google.com/uc?export=view&id=1pm9A5PV-a-VKA1r5Qv1GWN4UzbLipIuq',
+        profilePhotoBrand: profileBrandDisneyStar,
+        // profilePhotoBrand: 'https://drive.google.com/uc?export=view&id=1M2jeNUiF4PlXHauwL1tveCW3u70kHRuW',
         freeDays: null,
         title: 'Sin cargo con el nivel 6',
         brandName: 'Disney+ y Star+',
     },
     {
         id: 2,
-        backgroundPhoto: 'https://drive.google.com/uc?export=view&id=1BVfX-ht5fn8JJrEm2TRdcQwEVUtUgSlF',
-        profilePhotoBrand: 'https://drive.google.com/uc?export=view&id=1dTH4-9gMIhscJVINQ_lyyI3y-UEv3VBr',
+        backgroundPhoto: bgParamount,
+        // backgroundPhoto: 'https://drive.google.com/uc?export=view&id=1BVfX-ht5fn8JJrEm2TRdcQwEVUtUgSlF',
+        profilePhotoBrand: profileBrandParamount,
+        // profilePhotoBrand: 'https://drive.google.com/uc?export=view&id=1dTH4-9gMIhscJVINQ_lyyI3y-UEv3VBr',
         freeDays: '7 DÍAS GRATIS',
         title: 'Hasta 50% OFF',
         brandName: 'Paramount+',
     },
     {
         id: 3,
-        backgroundPhoto: 'https://drive.google.com/uc?export=view&id=1HqIwJiyAHfjWCXQXLsQFFPyGzL0wPdwm',
-        profilePhotoBrand: 'https://drive.google.com/uc?export=view&id=1T7qKN2TXWDOa1QQmapaJHeoKMrXndRAG',
+        backgroundPhoto: bgHBO,
+        // backgroundPhoto: 'https://drive.google.com/uc?export=view&id=1HqIwJiyAHfjWCXQXLsQFFPyGzL0wPdwm',
+        profilePhotoBrand: profileBrandHBO,
+        // profilePhotoBrand: 'https://drive.google.com/uc?export=view&id=1T7qKN2TXWDOa1QQmapaJHeoKMrXndRAG',
         freeDays: '7 DÍAS GRATIS',
         title: 'Hasta 50% OFF',
         brandName: 'HBO Max',
--- src/api/mock-offers.ts
+++ src/api/mock-offers.ts
@@ -1,3 +1,9 @@
+import product1 from '../assets/products/product1-offers.webp';
+import product2 from '../assets/products/product2-offers.webp';
+import product3 from '../assets/products/product3-offers.png';
+import product4 from '../assets/products/product4-offers.webp';
+import product5 from '../assets/products/product5-offers.webp';
+
 export const mockOffers = [
     {
         id: 1,
@@ -6,7 +12,8 @@ export const mockOffers = [
         originalPrice: '157.358',
         description: 'Amazon Fire TV Stick Lite de voz Full HD 8GB negro con 1GB',
         freeSend: true,
-        urlImage: 'https://drive.google.com/uc?export=view&id=1Xh4w8JAdmPt5k1vS4OFlcgQl3pJDc3YH'
+        urlImage: product1
+        // urlImage: 'https://drive.google.com/uc?export=view&id=1Xh4w8JAdmPt5k1vS4OFlcgQl3pJDc3YH'
     },
     {
         id: 2,
@@ -15,7 +22,8 @@ export const mockOffers = [
         originalPrice: '1.899.358',
         description: 'Caminadora Electrica Banda Trotadora 1.75hp 3niveles Inclina',
         freeSend: false,
-        urlImage: 'https://drive.google.com/uc?export=view&id=1ttEJ8fSVvyFJCKkxIYsQAupMuOHZFf8Z'
+        urlImage: product2
+        // urlImage: 'https://drive.google.com/uc?export=view&id=1ttEJ8fSVvyFJCKkxIYsQAupMuOHZFf8Z'
     },
     {
         id: 3,
@@ -24,7 +32,8 @@ export const mockOffers = [
         originalPrice: '1.499.900',
         description: 'Bicicleta Montaña Todoterreno Rin 29 Shimano 21 Velocidades',
         freeSend: true,
-        urlImage: 'https://drive.google.com/uc?export=view&id=1Fighg9WHP8vEqMA0mnNQ_B3CIzcKnQRm'
+        urlImage: product3
+        // urlImage: 'https://drive.google.com/uc?export=view&id=1Fighg9WHP8vEqMA0mnNQ_B3CIzcKnQRm'
     },
     {
         id: 4,
@@ -33,7 +42,8 @@ export const mockOffers = [
         originalPrice: '64.800',
         description: 'Difusor De Aromas 500 Ml Control Remoto Humidificador ',
         freeSend: false,
-        urlImage: 'https://drive.google.com/uc?export=view&id=1U7tW7T5d5HRQHTXv-UepJbDrkP7Y5Wbq'
+        urlImage: product4
+        // urlImage: 'https://drive.google.com/uc?export=view&id=1U7tW7T5d5HRQHTXv-UepJbDrkP7Y5Wbq'
     },
     {
         id: 5,
@@ -42,6 +52,7 @@ export const mockOffers = [
         originalPrice: '1.200.000',
         description: 'Laptop HP 240 G7 gris 14", Intel Celeron N4020 8GB de RAM',
         freeSend: true,
-        urlImage: 'https://drive.google.com/uc?export=view&id=10zeNsOSxxm-jIWw0JssPGik3Y_U34LMQ'
+        urlImage: product5
+        // urlImage: 'https://drive.google.com/uc?export=view&id=10zeNsOSxxm-jIWw0JssPGik3Y_U34LMQ'
     },
 ];
