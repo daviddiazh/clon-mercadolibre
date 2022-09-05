9e68201ffe6ffa8e56be4d7d3923771f030f215c
daviddiazh <daviddiazlds1221@gmail.com>
2022-09-04 20:53:49 -0500

    Adding data at productID page

--- src/pages/ProductId.tsx
+++ src/pages/ProductId.tsx
@@ -2,8 +2,18 @@ import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
 import { Loading } from "../components/Loading/Loading";
 import { useFetchByID } from '../hooks/useFetchByID';
+import { AiFillStar } from 'react-icons/ai';
 
 export const ProductId = () => {
+
+    /**
+     * https://api.mercadolibre.com/items/MLC615879515 productID
+     * https://api.mercadolibre.com/items/MLC615879515/description description
+     * https://api.mercadolibre.com/users/550063615 shopID
+     * https://api.mercadolibre.com/sites/MLC/search?q=iphone search
+     * 
+     * otro clone: https://meli-practice-client.vercel.app/items/MLA931172973 - endpoints: https://github.com/facuperezbri/meli-practice-api/blob/main/index.js
+    **/
 
     const location = useLocation();
     const locationSplit = location.pathname.split('/product/')[1];
@@ -11,6 +21,8 @@ export const ProductId = () => {
     console.log( locationSplit )
 
     const { data, isLoading, hasError } = useFetchByID(`https://api.mercadolibre.com/items/${ locationSplit }`);
+
+    const rate = Array.from({length: 5})
 
     console.log('DATA BY PRODUCT ID: ', data);
     console.log('title: ', data.title);
@@ -32,7 +44,19 @@ export const ProductId = () => {
 
     return (
         <div>
-
+            <p>{ data?.condition === 'new' ? 'Nuevo' : 'Reacondicionado' } | { data?.sold_quantity } vendidos</p>
+            <h1>{ data?.title }</h1>
+            <del>$ { data?.original_price! > data?.price! ? data?.original_price : null }</del>
+            <p>$ { data?.price }</p>
+            <div>
+                {
+                    rate.map(item => (
+                        <span style={{ color: "#3483fa" }}>
+                            <AiFillStar />
+                        </span>
+                    ))
+                }
+            </div>
         </div>
     )
 }
