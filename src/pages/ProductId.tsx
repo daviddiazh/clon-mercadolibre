e22adbe7816efec4e5649b436f4ae8ae0034bb81
daviddiazh <daviddiazlds1221@gmail.com>
2022-09-09 22:04:34 -0500

    Adding styles and details data (Info prouductId page)

--- src/pages/ProductId.tsx
+++ src/pages/ProductId.tsx
@@ -8,7 +8,11 @@ import visa from '../assets/logos-credit-cards/visa.png';
 import mcard from '../assets/logos-credit-cards/mcard.png';
 import amexpress from '../assets/logos-credit-cards/amexpress.jpg';
 
-import '../components/pages/ProductId.css'
+import { TbTruckDelivery } from 'react-icons/tb';
+import { IoReturnDownBackOutline } from 'react-icons/io5';
+
+import '../components/pages/ProductId.css';
+import { BiChevronDown } from "react-icons/bi";
 
 export const ProductId = () => {
 
@@ -56,21 +60,48 @@ export const ProductId = () => {
                 </div>
 
                 <div className="product-content">
-                    <div className="product-image">
+                    <div className="product-data">
                         <img src={data?.pictures?.[0].url} alt={data?.title} />
+                        
+                        <div className="">
+
+                        </div>
                     </div>
+
                     <div className="product-info">
+
                         <p className="condition-product">{ data?.condition === 'new' ? 'Nuevo' : 'Reacondicionado' } | { data?.sold_quantity } vendidos</p>
                         <h1 className="title-product">{ data?.title?.substring(0, 50) }</h1>
                         <del>{ data?.original_price! > data?.price! ? `$ ${data?.original_price}` : null }</del>
-                        <p>$ { data?.price }</p>
-                        <p>Hasta 48 cuotas</p>
+                        <p className="price-product">$ { data?.price }</p>
+                        <p style={{ lineHeight: '1px', marginTop: 12 }}>Hasta 48 cuotas</p>
                         <div className="logos-credit-cards">
                             <img className="logo-visa" src={visa} alt="Visa" />
                             <img className="logo-amexpress" src={amexpress} alt="American Express" />
                             <img className="logo-mcard" src={mcard} alt="Master Card" />
                         </div>
-                        <p>Más información</p>
+                        <p className="more-info">Más información</p>
+
+                        <div className="deliver-info">
+                            <span><TbTruckDelivery /></span>
+                            <p>Llega gratis el miercoles</p>
+                        </div>
+                        <p className="others-forms-receive">Ver más formas de entrega</p>
+
+                        <div className="return-free">
+                            <span><IoReturnDownBackOutline /></span>
+                            <p>Devolución gratis</p>
+                        </div>
+                        <p className="return-deliver">Tienes 30 días desde que lo recibes.</p>
+                        <p className="more-info-two">Conocer más</p>
+
+                        <div style={{ display: 'flex', alignItems: 'center', cursor: 'pointer' }}>
+                            <p>Cantidad: <span style={{ fontWeight: 600 }}>1 unidad</span></p>
+                            <span className="quantity"><BiChevronDown /></span>
+                        </div>
+
+                        <button className="btn-buy-now">Comprar ahora</button>
+                        <button className="btn-add-to-cart">Agregar al carrito</button>
                     </div>
                 </div>
 
