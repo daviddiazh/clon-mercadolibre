ef102d3d20ba3805fd3a971159a1325d18edee29
daviddiazh <daviddiazlds1221@gmail.com>
2022-09-11 04:05:22 -0500

    Change styles on Footer Component

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -19,20 +19,22 @@ export const Footer = () => {
                         <span><IoIosArrowUp /></span>
                     </button>
                 </div>
-                <div className="container-footer">
-                    <div className="mock-links">
-                        <p>Trabaja con nosotros</p>
-                        <p>Terminos y condiciones</p>
-                        <p>Como cuidamos tu privacidad</p>
-                        <p>Accesibilidad</p>
-                        <p>Ayuda / PQR</p>
-                        <p>www.sic.gov.co</p>
-                    </div>
-                    <div className="copyright">
-                        <p>Copyright © 1999-{ currentYear } MercadoLibre Colombia LTDA.</p>
-                        <p>Carrera 17 Numero 93 - 09 Piso 3, Bogotá D.C., Colombia</p>
+                <div className="second-container-footer">
+                    <div className="container-footer">
+                        <div className="mock-links">
+                            <p>Trabaja con nosotros</p>
+                            <p>Terminos y condiciones</p>
+                            <p>Como cuidamos tu privacidad</p>
+                            <p>Accesibilidad</p>
+                            <p>Ayuda / PQR</p>
+                            <p>www.sic.gov.co</p>
+                        </div>
+                        <div className="copyright">
+                            <p>Copyright © 1999-{ currentYear } MercadoLibre Colombia LTDA.</p>
+                            <p>Carrera 17 Numero 93 - 09 Piso 3, Bogotá D.C., Colombia</p>
 
-                        <h3 style={{ textAlign: 'right', paddingTop: 10 }}>By. David Diaz H - { currentYear }</h3>
+                            <h3 style={{ textAlign: 'right', paddingTop: 10 }}>By. David Diaz H - { currentYear }</h3>
+                        </div>
                     </div>
                 </div>
             </div>
